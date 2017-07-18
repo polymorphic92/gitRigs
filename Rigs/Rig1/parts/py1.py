440da8ece8900e69c5b8a05e59d1597dd39dff77
kylebrah <kylebrah@buffalo.edu>
2017-07-18 10:46:13 -0400

    updated repo dir structure

--- Rigs/Rig1/parts/py1.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-# This is my first script.
-
-print "Hello Python"
--- Rigs/Rig1/rig1-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# A sample Bash script, by Ryan
-echo ${BASH_SOURCE/%parts/*}
-echo done testing 
-for file in ${BASH_SOURCE}/parts/*; do
-    [ -f "$file" ] && [ -x "$file" ] && "$file"
-done
